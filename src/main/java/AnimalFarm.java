e5dc3889eddc3a575d5c9cf47c1b968b35191437
Yana Shmygleva <shmyaniv@mail.ru>
2024-10-30 21:05:12 +0300

    Begin 30/10/2024

---
+++ src/main/java/AnimalFarm.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.List;
+
+
+public class AnimalFarm {
+  private List<String> farmAnimals;  // параметр - список строк
+
+  public AnimalFarm(List<String> farmAnimals) {   // конструктор с параметром
+    this.farmAnimals = farmAnimals;
+  }
+
+}
